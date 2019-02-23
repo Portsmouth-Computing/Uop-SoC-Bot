bfb13a205020bfe4eb4004cae9b2a60b074398ce
Hopson97 <mhopson@hotmail.co.uk>
2019-02-23 14:58:47 +0000

    readd the src files

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import discord
+import json
+
+from message_event import MessageEventHandler
+
+bot = discord.Client()
+message_event_handler = MessageEventHandler()
+
+with open("data/config.json") as file:
+    config = json.load(file)
+
+@bot.event
+async def on_ready():
+    print("Bot is now active")
+    game = discord.Game()
+    game.name = "Type '>help'"
+    await bot.change_presence(game = game)
+
+@bot.event
+async def on_message(message):
+    await message_event_handler.handle(bot, message)
+
+bot.run(config["id"])
---
+++ src/message_event.py
@@ -0,0 +1,17 @@
+import discord
+from command_handler import CommandHandler
+
+class MessageEventHandler:
+    def __init__(self):
+        self.command_handler = CommandHandler()
+
+    async def handle(self, bot: discord.Client, message: discord.Message):
+        content = message.content
+        channel = message.channel
+        #The ">" character in a mesage indicates it is a command
+        if content.startswith(">"):
+                content = content[1:]
+                args    = content.split(" ")
+                command = args[0]
+                await self.command_handler.handle(bot, channel, command, args)
+        

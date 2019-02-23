8dabe18b4b473e4e9a294e11db347b1882a2133b
Hopson97 <mhopson@hotmail.co.uk>
2019-02-23 14:56:38 +0000

    Move python sources into src/ directory

--- command_handler.py
+++
@@ -1,6 +0,0 @@
-import discord
-
-class CommandHandler:
-    async def handle(self, bot: discord.Client, channel: discord.Channel, command: str, args: [str]):
-        #Default/basic commands
-        await bot.send_message(channel, command + command)
